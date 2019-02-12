f4257b2462985c1b49ed4bb96e3c9dbae142f303
Mohzuned <45491623+Mohzuned@users.noreply.github.com>
2019-02-12 09:14:24 +0530

    Expenses-os-ify

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,4 +93,4 @@ FRAMEWORKS_BASE_SUBDIRS := \
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
 
-include $(TOPDIR)vendor/bootleggers/build/core/pathmap.mk
+include $(TOPDIR)vendor/expenses/build/core/pathmap.mk
